a311f4ccad0c6e3ce6f1633525162d40c2a44e7e
Moongazing <tunahan.ali.ozturk@outlook.com>
2025-01-07 17:38:28 +0100

    Add DocumentType enum and fix ExpenseType enum
    Added a new DocumentType enum in DocumentType.cs with various document types in the Moongazing.Empyrean.Domain.Enums namespace. Fixed the ExpenseType enum in ExpenseType.cs by adding a missing closing curly brace.

---
+++ Moongazing.Empyrean/src/Moongazing.Empyrean.Domain/Enums/DocumentType.cs
@@ -0,0 +1,24 @@
+﻿namespace Moongazing.Empyrean.Domain.Enums;
+
+public enum DocumentType
+{
+    Passport,
+    Visa,
+    NationalId,
+    BirthCertificate,
+    ProfilePicture,
+    HealthReport,
+    DriverLicense,
+    EmploymentContract,
+    Diploma,
+    WorkPermit,
+    InsuranceCertificate,
+    UtilityBill,
+    BankStatement,
+    TaxDocument,
+    ResidencePermit,
+    MarriageCertificate,
+    AcademicTranscript,
+    CriminalRecord,
+    Other
+}
