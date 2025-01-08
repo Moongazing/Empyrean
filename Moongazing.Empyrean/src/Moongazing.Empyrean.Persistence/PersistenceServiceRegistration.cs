fe1585a749410b91279fd7af33dfe675d23ed0e2
Moongazing <tunahan.ali.ozturk@outlook.com>
2025-01-08 11:48:09 +0100

    Refactor logging, add PostgreSQL support
    Significantly refactored `GraylogLogger.cs` by removing all using directives and the entire `GraylogLogger` class, including its configuration and initialization. Removed `GraylogSample.yml`, which contained Docker Compose configuration for Graylog, MongoDB, and Elasticsearch.
    
    Added `Npgsql.EntityFrameworkCore.PostgreSQL` version `9.0.2` to `Moongazing.Empyrean.Persistence.csproj`, indicating a switch to PostgreSQL as the database provider.
    
    Introduced `PersistenceServiceRegistration.cs` with a static class `PersistenceServiceRegistration` and method `AddPersistenceServices` to register services:
    - Retry policy using Polly for `SqlException`.
    - Configured `BaseDbContext` to use PostgreSQL with a connection string from the configuration.
    - Added HTTP context accessor.
    - Added database migration applier using `BaseDbContext`.

---
+++ Moongazing.Empyrean/src/Moongazing.Empyrean.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moongazing.Empyrean.Persistence.Contexts;
+using Moongazing.Kernel.Persistence.MigrationApplier;
+using Polly;
+using Policy = Polly.Policy;
+
+namespace Moongazing.Empyrean.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        var retryPolicy = Policy
+                         .Handle<SqlException>()
+                         .WaitAndRetry(
+                         [
+                            TimeSpan.FromSeconds(10),
+                            TimeSpan.FromSeconds(20),
+                            TimeSpan.FromSeconds(30),
+                         ]);
+
+        services.AddDbContext<BaseDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("EmpyreanDb")));
+
+        services.AddHttpContextAccessor();
+
+        services.AddDbMigrationApplier(buildServices => buildServices.GetRequiredService<BaseDbContext>());
+
+
+
+        return services;
+    }
+
+
+}
