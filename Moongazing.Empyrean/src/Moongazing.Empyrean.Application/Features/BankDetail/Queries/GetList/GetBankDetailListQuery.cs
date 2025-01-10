9985b85f8b457c1900265c4fac535a640d52f105
Moongazing <tunahan.ali.ozturk@outlook.com>
2025-01-10 14:01:01 +0100

    GetList

---
+++ Moongazing.Empyrean/src/Moongazing.Empyrean.Application/Features/BankDetail/Queries/GetList/GetBankDetailListQuery.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Moongazing.Empyrean.Application.Repositories;
+using Moongazing.Empyrean.Domain.Entities;
+using Moongazing.Kernel.Application.Pipelines.Authorization;
+using Moongazing.Kernel.Application.Pipelines.Caching;
+using Moongazing.Kernel.Application.Pipelines.Logging;
+using Moongazing.Kernel.Application.Pipelines.Performance;
+using Moongazing.Kernel.Application.Requests;
+using Moongazing.Kernel.Application.Responses;
+using Moongazing.Kernel.Persistence.Paging;
+using Moongazing.Kernel.Security.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Moongazing.Empyrean.Application.Features.BankDetails.Constants.BankDetailOperationClaims;
+
+namespace Moongazing.Empyrean.Application.Features.BankDetail.Queries.GetList;
+
+public class GetBankDetailListQuery : IRequest<GetListResponse<GetBankDetailListResponse>>,
+    ILoggableRequest, ICachableRequest, ISecuredRequest, IIntervalRequest
+
+{
+    public PageRequest PageRequest { get; set; } = default!;
+    public string[] Roles => [Admin, Read, GeneralOperationClaims.Read];
+    public string CacheKey => $"{GetType().Name}({PageRequest.PageIndex}-{PageRequest.PageSize})";
+    public bool BypassCache { get; }
+    public string? CacheGroupKey => "BankDetails";
+    public TimeSpan? SlidingExpiration { get; }
+    public int Interval => 15;
+
+    public class GetBankDetailListQueryHandler : IRequestHandler<GetBankDetailListQuery, GetListResponse<GetBankDetailListResponse>>
+    {
+        private readonly IBankDetailRepository bankDetailRepository;
+        private readonly IMapper mapper;
+
+        public GetBankDetailListQueryHandler(IBankDetailRepository bankDetailRepository,
+                                             IMapper mapper)
+        {
+            this.bankDetailRepository = bankDetailRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<GetListResponse<GetBankDetailListResponse>> Handle(GetBankDetailListQuery request, CancellationToken cancellationToken)
+        {
+            IPagebale<BankDetailEntity> bankDetailList = await bankDetailRepository.GetListAsync(
+               index: request.PageRequest.PageIndex,
+               size: request.PageRequest.PageSize,
+               include: x => x.Include(x => x.Employee),
+               cancellationToken: cancellationToken);
+
+            GetListResponse<GetBankDetailListResponse> response = mapper.Map<GetListResponse<GetBankDetailListResponse>>(bankDetailList);
+
+            return response;
+        }
+    }
+}
---
+++ Moongazing.Empyrean/src/Moongazing.Empyrean.Application/Features/BankDetail/Queries/GetList/GetBankDetailListResponse.cs
@@ -0,0 +1,10 @@
+﻿using Moongazing.Kernel.Application.Responses;
+
+public class GetBankDetailListResponse : IResponse
+{
+    public Guid Id { get; set; }
+    public string BankName { get; set; } = default!;
+    public string AccountNumber { get; set; } = default!;
+    public string IBAN { get; set; } = default!;
+    public Guid EmployeeId { get; set; }
+}
