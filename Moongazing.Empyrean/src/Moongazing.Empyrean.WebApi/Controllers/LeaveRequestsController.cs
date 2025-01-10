d4e906da07810fdfcce240605c4820d7af15f25b
Moongazing <tunahan.ali.ozturk@outlook.com>
2025-01-10 17:55:02 +0100

    DeleteLeaeveRequest

--- Moongazing.Empyrean/src/Moongazing.Empyrean.WebApi/Controllers/LeaveRequestsController.cs
+++ Moongazing.Empyrean/src/Moongazing.Empyrean.WebApi/Controllers/LeaveRequestsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Moongazing.Empyrean.Application.Features.BankDetails.Commands.Create;
 using Moongazing.Empyrean.Application.Features.LeaveRequests.Commands.Create;
+using Moongazing.Empyrean.Application.Features.LeaveRequests.Commands.Delete;
 using Moongazing.Empyrean.WebApi.Controllers.Common;
 
 [Route("api/empyrean/leaverequests")]
@@ -12,6 +13,13 @@ public sealed class LeaveRequestsController : BaseController
     public async Task<IActionResult> Add([FromBody] CreateLeaveRequestCommand createLeaveRequestCommand)
     {
         CreateLeaveRequestResponse result = await Sender.Send(createLeaveRequestCommand).ConfigureAwait(false);
+        return Ok(result);
+    }
+
+    [HttpDelete("delete")]
+    public async Task<IActionResult> Delete ([FromBody] DeleteLeaveRequestCommand deleteLeaveRequestCommand)
+    {
+        DeleteLeaveRequestResponse result = await Sender.Send(deleteLeaveRequestCommand).ConfigureAwait(false);
         return Ok(result);
     }
 }
