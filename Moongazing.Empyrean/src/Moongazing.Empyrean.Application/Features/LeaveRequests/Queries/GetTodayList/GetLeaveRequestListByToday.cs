5d83c8bc884678539da41b5abbe04cfdda0325b5
Moongazing <tunahan.ali.ozturk@outlook.com>
2025-01-11 14:53:34 +0100

    getTodayLeaveRequestListQuery

---
+++ Moongazing.Empyrean/src/Moongazing.Empyrean.Application/Features/LeaveRequests/Queries/GetTodayList/GetLeaveRequestListByToday.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Moongazing.Empyrean.Application.Features.BankDetails.Constants;
+using Moongazing.Empyrean.Application.Features.BankDetails.Queries.GetListByDynamic;
+using Moongazing.Empyrean.Application.Features.LeaveRequests.Constants;
+using Moongazing.Empyrean.Application.Repositories;
+using Moongazing.Empyrean.Domain.Entities;
+using Moongazing.Kernel.Application.Pipelines.Authorization;
+using Moongazing.Kernel.Application.Pipelines.Caching;
+using Moongazing.Kernel.Application.Pipelines.Logging;
+using Moongazing.Kernel.Application.Pipelines.Performance;
+using Moongazing.Kernel.Application.Requests;
+using Moongazing.Kernel.Application.Responses;
+using Moongazing.Kernel.Persistence.Paging;
+using Moongazing.Kernel.Security.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Moongazing.Empyrean.Application.Features.LeaveRequests.Constants.LeaveRequestOperationClaims;
+
+namespace Moongazing.Empyrean.Application.Features.LeaveRequests.Queries.GetTodayList;
+
+public class GetLeaveRequestListByTodayQuery : IRequest<GetListResponse<GetLeaveRequestListByTodayResponse>>,
+     ILoggableRequest, ICachableRequest, ISecuredRequest, IIntervalRequest
+{
+    public PageRequest PageRequest { get; set; } = default!;
+    public string[] Roles => [Admin, Read, GeneralOperationClaims.Read];
+    public string CacheKey => $"{GetType().Name}({PageRequest.PageIndex}-{PageRequest.PageSize})";
+    public bool BypassCache { get; }
+    public string? CacheGroupKey => LeaveRequestMessages.SectionName;
+    public TimeSpan? SlidingExpiration { get; }
+    public int Interval => 15;
+
+
+    public class GetLeaveRequestListByTodayQueryHandler : IRequestHandler<GetLeaveRequestListByTodayQuery, GetListResponse<GetLeaveRequestListByTodayResponse>>
+    {
+
+        private readonly ILeaveRequestRepository leaveRequestRepository;
+        private readonly IMapper mapper;
+
+        public GetLeaveRequestListByTodayQueryHandler(ILeaveRequestRepository leaveRequestRepository,
+                                                      IMapper mapper)
+        {
+            this.leaveRequestRepository = leaveRequestRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<GetListResponse<GetLeaveRequestListByTodayResponse>> Handle(GetLeaveRequestListByTodayQuery request, CancellationToken cancellationToken)
+        {
+            var today = DateTime.Today;
+
+            IPagebale<LeaveRequestEntity> leaveRequestList = await leaveRequestRepository.GetListAsync(
+                predicate: x => x.StartDate <= today && x.EndDate >= today && x.IsApproved,
+                index: request.PageRequest.PageIndex,
+                size: request.PageRequest.PageSize,
+                include: x => x.Include(x => x.Employee),
+                cancellationToken: cancellationToken);
+
+            GetListResponse<GetLeaveRequestListByTodayResponse> response = mapper.Map<GetListResponse<GetLeaveRequestListByTodayResponse>>(leaveRequestList);
+
+            return response;
+        }
+
+    }
+}
--- Moongazing.Empyrean/src/Moongazing.Empyrean.WebApi/Controllers/LeaveRequestsController.cs
+++ Moongazing.Empyrean/src/Moongazing.Empyrean.WebApi/Controllers/LeaveRequestsController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Moongazing.Empyrean.Application.Features.BankDetail.Queries.GetList;
 using Moongazing.Empyrean.Application.Features.LeaveRequests.Commands.Create;
 using Moongazing.Empyrean.Application.Features.LeaveRequests.Commands.Delete;
 using Moongazing.Empyrean.Application.Features.LeaveRequests.Commands.Update;
+using Moongazing.Empyrean.Application.Features.LeaveRequests.Queries.GetTodayList;
 using Moongazing.Empyrean.WebApi.Controllers.Common;
+using Moongazing.Kernel.Application.Requests;
+using Moongazing.Kernel.Application.Responses;
 
 [Route("api/empyrean/leaverequests")]
 [ApiController]
@@ -17,7 +21,7 @@ public sealed class LeaveRequestsController : BaseController
     }
 
     [HttpDelete("delete")]
-    public async Task<IActionResult> Delete ([FromBody] DeleteLeaveRequestCommand deleteLeaveRequestCommand)
+    public async Task<IActionResult> Delete([FromBody] DeleteLeaveRequestCommand deleteLeaveRequestCommand)
     {
         DeleteLeaveRequestResponse result = await Sender.Send(deleteLeaveRequestCommand).ConfigureAwait(false);
         return Ok(result);
@@ -26,6 +30,13 @@ public sealed class LeaveRequestsController : BaseController
     public async Task<IActionResult> Edit([FromBody] UpdateLeaveRequestCommand editLeaveRequestCommand)
     {
         UpdateLeaveRequestResponse result = await Sender.Send(editLeaveRequestCommand).ConfigureAwait(false);
+        return Ok(result);
+    }
+    [HttpGet("getlist/today")]
+    public async Task<IActionResult> GetListToday([FromQuery] PageRequest pageRequest)
+    {
+        GetLeaveRequestListByTodayQuery getTodayLeaveRequestListQuery = new() { PageRequest = pageRequest };
+        GetListResponse<GetLeaveRequestListByTodayResponse> result = await Sender.Send(getTodayLeaveRequestListQuery).ConfigureAwait(false);
         return Ok(result);
     }
 }
