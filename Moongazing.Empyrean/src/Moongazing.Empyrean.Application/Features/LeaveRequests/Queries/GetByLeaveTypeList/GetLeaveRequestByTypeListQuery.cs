3bbdea69569abc33da372d571deff1d8b25f0649
Moongazing <tunahan.ali.ozturk@outlook.com>
2025-01-12 18:39:39 +0100

    fixes

---
+++ Moongazing.Empyrean/src/Moongazing.Empyrean.Application/Features/LeaveRequests/Queries/GetByLeaveTypeList/GetLeaveRequestByTypeListQuery.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Moongazing.Empyrean.Application.Features.LeaveRequests.Constants;
+using Moongazing.Empyrean.Domain.Enums;
+using Moongazing.Kernel.Application.Requests;
+using Moongazing.Kernel.Security.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Moongazing.Empyrean.Application.Features.LeaveRequests.Constants.LeaveRequestOperationClaims;
+
+namespace Moongazing.Empyrean.Application.Features.LeaveRequests.Queries.GetByLeaveTypeList;
+
+public class GetLeaveRequestByTypeListQuery : IRequest<GetLeaveRequestByTypeListResponse>
+{
+    public LeaveType LeaveType { get; set; }
+    public PageRequest PageRequest { get; set; } = default!;
+    public string[] Roles => [Admin, Read, GeneralOperationClaims.Read];
+    public string CacheKey => $"{GetType().Name}({PageRequest.PageIndex}-{PageRequest.PageSize}/{LeaveType})";
+    public bool BypassCache { get; }
+    public string? CacheGroupKey => LeaveRequestMessages.SectionName;
+    public TimeSpan? SlidingExpiration { get; }
+    public int Interval => 15;
+}
---
+++ Moongazing.Empyrean/src/Moongazing.Empyrean.Application/Features/LeaveRequests/Queries/GetByLeaveTypeList/GetLeaveRequestByTypeListResponse.cs
@@ -0,0 +1,16 @@
+﻿using Moongazing.Empyrean.Domain.Enums;
+using Moongazing.Kernel.Application.Responses;
+
+namespace Moongazing.Empyrean.Application.Features.LeaveRequests.Queries.GetByLeaveTypeList;
+
+public class GetLeaveRequestByTypeListResponse : IResponse
+{
+    public Guid Id { get; set; }
+    public Guid EmployeeId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public LeaveType LeaveType { get; set; } = default!;
+    public string Reason { get; set; } = default!;
+    public bool IsApproved { get; set; }
+    public string ApprovedBy { get; set; } = default!;
+}
